6e76971c6b39cbb510320180ed735e5a69dc6262
izzet ertas <izzet.ertas@vodafone.com>
2018-08-23 12:50:48 +0100

    logo was added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
 import Calculator from './Calculator'
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Calculator />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Calculator />
+      </div>
+      
     );
   }
 }
